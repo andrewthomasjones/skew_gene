ac7a7e7e5d9e1f4cd31de70b530c008be42bb155
Andrew Jones <uqajon14@d-i184-60-119.staff.eduroam.uq.edu.au>
2017-06-23 16:26:46 +1000

    refactor

--- main_file.R
+++ main_file.R
@@ -1,12 +1,14 @@
 library(EMMIXcskew)
 library(EMMIXskew)
-
+library(ggplot2)
 library(mnormt)
 library(locfdr)
 library(qvalue)
 library(cepp)
-source("fmcfust.R")
-source("fmmst.R")
+library(mixtools)
+source("fit.R")
+source("util.R")
+source("density.R")
 
 
 data("hedenfalk")
@@ -21,19 +23,37 @@ Colon$z<-qnorm(1-Colon$p)
 
 data("hivdata")# already pval
 
+x<-fmcfust((hedenfalk$z),1,   emp=T)
+y<-fmcfust((hivdata),3,   emp=T)
+z<-fmcfust(Colon$z,1,   emp=T)
 
-x<-fmcfust2((hedenfalk$z),1, verbose=T, emp=F)
-x2<-fmcfust2((hedenfalk$z),1, verbose=T, emp=T)
+x2<-fmcfust((hedenfalk$z),1,   emp=F)
+y2<-fmcfust((hivdata),3,   emp=F)
+z2<-fmcfust(Colon$z,1,   emp=F)
 
-y<-fmcfust2((hivdata),3, verbose=T, emp=T)
-z<-fmcfust2(Colon$z,1, verbose=T, emp=T, itmax=20)
+hiv_fit_norm <- normalmixEM(x=hivdata, lambda =pi_0(hivdata,3), k=2)
+hed_fit_norm <- normalmixEM(x=hedenfalk$z,k=2, lambda = pi_0(hedenfalk$z,0), mean.constr = c(0, NA), sd.constr=c(1, NA))
+col_fit_norm <- normalmixEM(x=Colon$z, lambda =pi_0(Colon$z,0), k=2, mean.constr = c(0, NA), sd.constr=c(1, NA))
+
+plot_normal_densities(hiv_fit_norm,hivdata, 1000, 50, "Hiv Emp. Normal")
+plot_normal_densities(col_fit_norm,Colon$z, 1000, 50, "Colon Normal")
+plot_normal_densities(hed_fit_norm,hedenfalk$z, 1000, 50, "Hedenfalk Normal")
+
+plot_skew_densities(z, Colon$z, 1000, 50, "Colon Skew  Emp. Null")
+plot_skew_densities(y, hivdata, 1000, 50, "HIV Skew Emp. Null")
+plot_skew_densities(x, hedenfalk$z, 1000, 50, "Hedenfalk Skew  Emp. Null")
+plot_skew_densities(z2, Colon$z, 1000, 50, "Colon Skew")
+plot_skew_densities(y2, hivdata, 1000, 50, "HIV Skew")
+plot_skew_densities(x2, hedenfalk$z, 1000, 50, "Hedenfalk Skew")
+
 
 heden<-namer(x)
-log(length(hed_fit_norm$x))*3-2*hed_fit_norm$loglik
 FDR_etc(heden, 0.1)
+FDR_etc(y, 0.1)
+FDR_etc(z, 0.1)
 
-plot_skew_densities(z, Colon$z, 1000, 50, "Colon Skew")
-plot_skew_densities(y, hivdata, 1000, 50, "HIV Skew")
-plot_skew_densities(x, hedenfalk$z, 1000, 50, "Hedenfalk Skew")
+
+
+
 
 
--- util.R
+++ util.R
@@ -4,31 +4,6 @@ pi_0<-function(data, zeta){
 }
 
 
-fmcfust2 <- function(Y, zeta=1, emp,  initial=NULL, known=NULL, clust=NULL, itmax=100, eps=1e-6, nkmeans=20, verbose=T, method=c("moments","transformation","EMMIXskew","EMMIXuskew"), convergence=c("Aitken","likelihood","parameters")) {
-  g=2
-  q=1
-  Y<-as.matrix(Y)
-  p <- ncol(Y); n <- nrow(Y); fulldebug=F; 
-  q<-1
-  if (itmax > 1000) itmax <- 1000   #do not allow more than 1000 iterations (too much)
-  if(n > 5000 && p>=3) {
-    #        cat("  NOTE: For large datasets, please consider using EmSkew for faster computation.\n")
-    fulldebug = T
-  }
-  if(n <= 20*g) stop("sample size is too small!")      
-  
-  if(verbose) {
-    cat("Finite Mixture of Multivariate CFUST Distributions\n")
-    if(g==1) cat("with 1 component\n")
-    else cat("with ", g, "components\n")
-    cat("  ----------------------------------------------------\n\n")
-  }  
-  initial_1 <-fmcfust(2, Y, initial =list(pro=pi_0(Y,zeta)), itmax=10, verbose=F)
-  initial_1$pro<-pi_0(Y,zeta)
-  ndelta<-4
-  res <- fmfustt2( Y, initial_1, ndelta, emp, itmax=itmax)
-  return(res)
-}
 
 normal_densities<-function(fit, k){
   fit$G<-length(fit$lambda)
@@ -44,7 +19,7 @@ normal_densities<-function(fit, k){
   return(y)
 }
 
-plot_normal_densities<-function(fit,data, k, bins,name){
+plot_normal_densities<-function(fit, data, k, bins,name){
   data1<-as.data.frame(normal_densities(fit,k))
   data2<-data.frame(data=data)
   p=ggplot(data=data1)+geom_histogram(data=data2, aes(y=..density..,x=data), fill='white', colour='grey', bins=bins)+geom_line(aes(x=x,  y=y1))+geom_line(aes(x=x,  y=y2))+geom_line(data=data1, aes(x=x,  y=y1+y2))
@@ -52,22 +27,15 @@ plot_normal_densities<-function(fit,data, k, bins,name){
   print(p)
 }
 
-
-
-
-
 skew_densities<-function(fit,data, k){
   G<-length(fit$pro)
   y<-array(NA, c(k,G+2))
   xseq<-seq(min(data), max(data), length.out=k)
   y[,1]<-xseq
-  y[,2]<-dnorm(y[,1])*fit$pro[1]
+  y[,2]<-dnorm(y[,1], mean=fit$mu[[1]], sd=fit$sigma[[1]])*fit$pro[1]
   for(i in 3:(G+2)){
     y[,i]<-fit$pro[i-2]*dcfust(dat=as.matrix(xseq,nrow=1), mu=as.vector(fit$mu[[i-2]]), sigma=fit$sigma[[i-2]], delta=fit$delta[[i-2]], dof=fit$dof[i-2])
-    
-    
   }
-  
   colnames(y)<-c("x", "norm", paste0('y', seq(1:G)))
   return(y)
 }
@@ -80,12 +48,6 @@ plot_skew_densities<-function(fit, data, k, bins, name){
   print(p)
 }
 
-plot_normal_densities<-function(fit, k, bins){
-  data1<-as.data.frame(normal_densities(fit,k))
-  p=ggplot(data=data1)+geom_histogram(data=data2, aes(y=..density..,x=data), fill='white', colour='grey', bins=bins)+geom_line(aes(x=x,  y=y1))+geom_line(aes(x=x,  y=y2))+geom_line(data=data1, aes(x=x,  y=y1+y2))
-  p=p+scale_colour_discrete(guide=FALSE)+theme_bw()+xlab("z-scores")+ylab("Density")
-  print(p)
-}
 
 namer<-function(fit){
   #tidy names on other vars too
@@ -114,3 +76,144 @@ FDR_etc<-function(fit, c0){
   
   return(list(N_r=N_r, FDR=FDR, FNDR=FNDR, FNR=FNR,FPR=FPR))
 }
+
+#
+#  EM algorithm for Mixture of Multivariate Canonical Fundamental Skew t-distributioins
+#  Package: EMMIX-cskew
+#  Version: 0.9-1
+#
+#  Code by S. X. Lee
+#  Updated on 07 Sep 2015
+#
+# Lee S.X. and Mclachlan, G.J. (2015) Finite mixtures of canonical fundamental 
+# skew t-distributions: the unification of the restricted and unrestricted 
+# skew t-mixture models. Statistics and Computing. doi:10.1007/s11222-015-9545-x
+#
+# The following code are adapted from other packages
+#
+################################################################################
+#  misc.r
+#                           Miscellaneous Tools
+#
+################################################################################
+
+#skewness
+skewness <- function (x, na.rm = FALSE){
+  if (is.matrix(x))
+    apply(x, 2, skewness, na.rm = na.rm)
+  else if (is.vector(x)) {
+    if (na.rm) x <- x[!is.na(x)]
+    n <- length(x)
+    (sum((x-mean(x))^3)/n)/(sum((x-mean(x))^2)/n)^(3/2)
+  }
+  else if (is.data.frame(x))
+    sapply(x, skewness, na.rm = na.rm)
+  else skewness(as.vector(x), na.rm = na.rm)
+}
+
+#mahalanobis
+mahalanobis. <- function(x, center, cov, inverted=FALSE, ...) {
+  x <- if(is.vector(x)) matrix(x, ncol=length(x)) else as.matrix(x)
+  x <- t(sweep(x, 2, center))# = (x - center)
+  retval <- colSums(x * if(inverted) cov%*%x else solve(cov, x, ...))
+  names(retval) <- rownames(x)
+  retval
+}
+
+mahalanobis <- function(x, center, cov, inverted=FALSE, ...){
+  x <- if(is.vector(x)) matrix(x, ncol=length(x)) else as.matrix(x)
+  x <- sweep(x, 2, center)# = (x - center)
+  if(!inverted)
+    cov <- solve(cov, ...)
+  retval <- rowSums((x%*%cov) * x)
+  names(retval) <- rownames(x)
+  retval
+}
+
+#
+isPosDef <- function(M) { 
+  if ( all(M == t(M) ) ) { 
+    if (  all(eigen(M)$values > 0) ) {TRUE}
+    else {FALSE} 
+  }else {FALSE}  
+} 
+
+#
+is.whole <- function(a) { 
+  (is.numeric(a) && floor(a)==a) ||
+    (is.complex(a) && floor(Re(a)) == Re(a) && floor(Im(a)) == Im(a))
+}
+
+
+#multivariate normal density
+dmn <- function(x, mu=rep(0,d), Sigma, log=FALSE) {
+  d  <- if(is.matrix(Sigma)) ncol(Sigma) else 1
+  if(d>1 & is.vector(x)) x <- matrix(x, 1, d)
+  n  <- if(d==1)  length(x) else nrow(x) 
+  X  <- t(matrix(x, nrow=n, ncol=d)) - mu
+  Q  <- apply((solve(Sigma)%*% X)* X, 2, sum) 
+  logDet <- sum(logb(abs(diag(qr(Sigma)[[1]]))))
+  logPDF <- as.vector(Q + d*logb(2*pi)+logDet)/(-2)
+  if(log) logPDF else exp(logPDF)
+}
+
+#multivariate t density
+dmt <- function(dat, mu, sigma, dof = Inf, log = FALSE) {
+  if (dof == Inf)  return(dmn(dat, mu, sigma, log = log))
+  dof <- round(dof)
+  d  <- if(is.matrix(sigma)) ncol(sigma) else 1
+  n  <- if(d==1) length(dat) else nrow(dat)
+  X <- t(matrix(dat, nrow = n, ncol = d)) - mu
+  Q <- apply((solve(sigma) %*% X) * X, 2, sum)
+  logDet <- sum(logb(abs(diag(qr(sigma)$qr))))
+  logPDF <- (lgamma((dof + d)/2) - 0.5 * (d * logb(pi * dof) + logDet)
+             - lgamma(dof/2) - 0.5 * (dof + d) * logb(1 + Q/dof))
+  if(log) logPDF else exp(logPDF)
+}
+
+
+#error rate
+error.rate <- function(clust1,clust2){
+  if((n=length(clust1))!=length(clust2))  stop("error: length not equal")
+  if( (g=length(table(clust1)))!=length(table(clust2))) stop("the number of clusters are not equal")
+  permute<-function(a){
+    n<-length(a)
+    if(n==1) f<-a
+    else{
+      nm<-gamma(n)
+      f<-array(0,c(n,n*nm))
+      j<-1
+      
+      for(i in a){
+        f[1, (j-1)*nm+1:nm]<-i
+        f[-1,(j-1)*nm+1:nm]<-permute(setdiff(a,i))
+        j<-j+1
+      }
+    }    
+    f
+  }
+  
+  id<-1:n
+  cmb<-permute(1:g)
+  nperm<-ncol(cmb)
+  rate<-rep(0,nperm)
+  
+  for(i in 1:nperm){
+    tmp<-rep(0,g)
+    tc<-rep(0,n)
+    for(j in 1:g)
+      tc[clust2==j]=cmb[j,i]
+    
+    for(j in 1:g){  
+      tmp1<-0 
+      for(k in (1:g)[-j])
+        tmp1<-tmp1+length(intersect(id[clust1==j],id[tc==k]))
+      tmp[j]<-tmp1
+    }
+    rate[i]<-sum(tmp)/n
+  }
+  min(rate)
+}
+
+
+
